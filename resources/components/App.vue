b991b44ba51a719813cf781ce475fb57af8a5e50
nnaa <nnaa99toooin@example.com>
2023-08-15 02:13:08 +0900

    add vuetify

--- resources/components/App.vue
+++ resources/components/App.vue
@@ -6,4 +6,12 @@ const message = ref('Hello World!')
 
 <template>
   <h1>{{ message }}</h1>
+  <v-btn block text class="text-transform py-4 my-2" color="primary">ボタン</v-btn>
+
+  <!-- <v-app>
+    <v-main>
+      <v-container><span class="font-weight-bold"> 変わるよ </span></v-container>
+    </v-main>
+  </v-app> -->
+  
 </template>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,15 @@
-import { createApp } from "vue";
+import { createApp } from 'vue'
+import App from '../components/App.vue'
 
-import App from "../components/App.vue";
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
-console.log("おはよう");
+const vuetify = createVuetify({
+  components,
+  directives,
+})
 
-const app = createApp(App);
-
-app.mount("#app");
+createApp(App).use(vuetify).mount('#app')
