9160edf5d6e4989e5f9b1947c823e99cf87561af
nnaa <nnaa99toooin@example.com>
2023-08-17 22:20:12 +0900

    ログイン機能の実装

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,9 @@ Route::post('/register',[\App\Http\Controllers\UserController::class,'register']
 
 Route::post('/logout',[\App\Http\Controllers\UserController::class,'logout'])->name('user.logout');
 
+Route::get('/login',[\App\Http\Controllers\UserController::class,'showLogin'])->name('login');
+Route::post('/login',[\App\Http\Controllers\UserController::class,'login']);
+
 Route::middleware('auth')->group(function (){
     Route::get('/profile',[\App\Http\Controllers\UserController::class,'profile'])->name('profile');
 });
