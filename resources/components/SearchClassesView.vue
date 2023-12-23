be7957bbfb6b420acde446968caccbc4bc50bfe2
yuki <b202154@hiroshima-u.ac.jp>
2023-12-23 18:39:17 +0900

    tm@

---
+++ resources/components/SearchClassesView.vue
@@ -0,0 +1,69 @@
+<script setup>
+import { ref } from 'vue'
+import vuetify from '../js/vuetify';
+
+// // デフォルトで開くタブを設定
+// let openTab = ref("tab-1")
+
+// //より詳しい条件で探すときにtrueになり、詳細な条件を表示
+// let showDetailedCondition = ref(false)
+
+// //候補を格納する場所
+// let items = ref(["item A", "item B","item C","item D","item E","item F","item G"])
+
+// // 開講場所
+// let placeList = ref(["item A", "item B","item C","item D","item E","item F","item G"])
+// let place = ref(null)
+// // 開講部局
+// let departmentList = ref(["item A", "item B","item C","item D","item E","item F","item G"])
+// let departmant = ref(null)
+// // 開講区分
+// let classificationList = ref(["item A", "item B","item C","item D","item E","item F","item G"])
+// let classification = ref(null)
+// // 開講曜日
+// let dayList = ref(["item A", "item B","item C","item D","item E","item F","item G"])
+// let day = ref(null)
+// // 開講時間
+// let timeList = ref(["item A", "item B","item C","item D","item E","item F","item G"])
+// let time = ref(null)
+// // 履修年次
+// let gradeList = ref(["item A", "item B","item C","item D","item E","item F","item G"])
+// let grade = ref(null)
+// // 総合評価
+// let totalEvaluationList = ref(["item A", "item B","item C","item D","item E","item F","item G"])
+// let totalEvaluation = ref(null)
+// // 単位取得のしやすさ
+// let easeToGetCreditList = ref(["item A", "item B","item C","item D","item E","item F","item G"])
+// let easeToGetCredit = ref(null)
+// // 面白さ
+// let interestingList = ref(["item A", "item B","item C","item D","item E","item F","item G"])
+// let interesting = ref(null)
+// // 教員の質
+// let qualityOfTeachersList = ref(["item A", "item B","item C","item D","item E","item F","item G"])
+// let qualityOfTeachers = ref(null)
+// // サポート体制
+// let supportSystemList = ref(["item A", "item B","item C","item D","item E","item F","item G"])
+// let supportSystem = ref(null)
+// // スキルが身につくか
+// let skillsAcquiredList = ref(["item A", "item B","item C","item D","item E","item F","item G"])
+// let skillsAcquired = ref(null)
+// // ラベルで検索
+// let labelsList = ref(["item A", "item B","item C","item D","item E","item F","item G"])
+// let labels = ref([])
+
+// 講義コード
+// let lectureCodeList = ref(["item A", "item B","item C","item D","item E","item F","item G"])
+// let lectureCode = ref(null)
+
+// onMounted(() => {
+//     console.log("This is SearchClasssesView component")
+// })
+onMounted(() => {
+    console.log("This is onMounted")
+})
+</script>
+
+<template>
+    <h1>This is SearchClassesView</h1>
+</template>
+
--- resources/views/WelcomeView.vue
+++ resources/views/WelcomeView.vue
@@ -1,7 +1,12 @@
 <script setup>
 import Welcome from '../components/Welcome.vue'
+// import SearchClassesView from '../components/SearchClassesView.vue';
+onMounted(() => {
+    console.log("this is onmounted in welcome")
+})
 </script>
 
 <template>
     <Welcome></Welcome>
+    <!-- <SearchClassesView></SearchClassesView> -->
 </template>
