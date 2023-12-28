20d5c43cc5a3d9b67471e35565c027706115eaed
yuki <b202154@hiroshima-u.ac.jp>
2023-12-28 13:09:03 +0900

    検索結果に出てくる、授業一覧リストを作成

--- resources/components/ClassList.vue
+++ resources/components/ClassList.vue
@@ -1,18 +1,75 @@
 <script setup>
-import { ref } from 'vue'
-import vuetify from '../js/vuetify';
-import axios from 'axios';
-import { useRouter } from 'vue-router'
+import { ref } from "vue";
+import vuetify from "../js/vuetify";
+import axios from "axios";
+import { useRouter } from "vue-router";
+import ClassSummary from "./ClassSummary.vue";
 
+// classDataListに授業情報を外部から取得してくる
+const classDataList = ref([
+  {
+    className: "一攫千金特論",
+    classCode: "KA10000",
+    teacherName: "服部淳生",
+    totalEvaluation: "5",
+    numberOfReviews: "70000",
+  },
+  {
+    className: "一攫二千金特論",
+    classCode: "KA10001",
+    teacherName: "服部淳生",
+    totalEvaluation: "5",
+    numberOfReviews: "70000",
+  },
+  {
+    className: "一攫三千金特論",
+    classCode: "KA10003",
+    teacherName: "服部淳生",
+    totalEvaluation: "5",
+    numberOfReviews: "70000",
+  },
+  {
+    className: "エヴァンゲリオン特論",
+    classCode: "KA10004",
+    teacherName: "田中恒成",
+    totalEvaluation: "5",
+    numberOfReviews: "70000",
+  },
+  {
+    className: "おいしいコーヒーの入れ方",
+    classCode: "KA10005",
+    teacherName: "田中恒成",
+    totalEvaluation: "5",
+    numberOfReviews: "70000",
+  },
+  {
+    className: "君主論",
+    classCode: "KA10006",
+    teacherName: "Niccolò Machiavelli",
+    totalEvaluation: "5",
+    numberOfReviews: "70000",
+  },
+  {
+    className: "罪と罰",
+    classCode: "KA10007",
+    teacherName: "Фёдор Mихáйлович Достоéвский",
+    totalEvaluation: "5",
+    numberOfReviews: "70000",
+  },
+  {
+    className: "マルクス経済学",
+    classCode: "KA10008",
+    teacherName: "Karl Marx",
+    totalEvaluation: "5",
+    numberOfReviews: "70000",
+  },
+]);
 </script>
 
 <template>
-    <v-app>
-        <v-main>
-            <v-container class="bg-green-lighten-3 rounded-lg" height="400px">
-                <p>ここに記述をしていく</p>
-                <p>ClassList</p>
-            </v-container>
-        </v-main>
-    </v-app>
+  <v-container class="d-flex flex-column align-center ga-5">
+    <v-container v-for="n in classDataList.length" :key="n">
+      <ClassSummary class="mx-auto" :classData="classDataList[n - 1]"></ClassSummary>
+    </v-container>
+  </v-container>
 </template>
---
+++ resources/components/ClassSummary.vue
@@ -0,0 +1,45 @@
+<script setup>
+import { ref } from "vue";
+import vuetify from "../js/vuetify";
+import axios from "axios";
+import { useRouter } from "vue-router";
+
+const props = defineProps(["classData"])
+
+const moveToReview = () => {
+  console.log("moveToReview")
+}
+</script>
+
+
+<template>
+  <v-card class="border" max-width="800">
+        <v-container class="d-flex flex-row align-center justify-space-around">
+          <v-container class="ma-2 pa-0">
+            <a href="#" class="text-decoration-underline text-blue text-h6">
+              {{ classData.className }}</a
+            >
+          </v-container>
+          <v-container class="ma-0 pa-0"
+            >{{ classData.teacherName }} 先生</v-container
+          >
+          <v-container class="ma-0 pa-0">
+            <v-container
+              class="d-flex flex-row align-center justify-end ma-0 pa-0"
+              >
+              <!-- ここには星のコンポーネントを別で読み込む予定 -->
+              <div>★★★★★</div>
+              <div>({{ classData.numberOfReviews }})</div></v-container
+            >
+            <v-container
+              class="d-flex flex-row align-center justify-end ma-0 pa-0"
+              ><v-btn color="blue" @click="moveToReview"
+                ><v-icon>mdi-comment-plus-outline</v-icon>
+                <p>レビューする
+                </p></v-btn
+              ></v-container
+            >
+          </v-container>
+        </v-container>
+      </v-card>
+</template>
