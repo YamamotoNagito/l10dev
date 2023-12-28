ffd53aecfca732cedc081f1c4b771c70f4ea166e
Kosei Tanaka <k-sei0213@hotmail.com>
2023-12-28 23:15:40 +0900

    フッターに新デザインを適用した

--- resources/components/Footer.vue
+++ resources/components/Footer.vue
@@ -1,79 +1,39 @@
 <template>
-    <div>
-      <div class="wrapper">
-          <nav>
+    <v-app>
+      <v-footer class="bg-grey-lighten-1" app>
+        <v-row justify="center" no-gutters>
+          <v-btn 
+            href="/about-us"
+            variant="text"
+            rounded="xl" >
             Developed by SOS団
-            <a href="/TermsOfService">利用規約</a>
-            <a href="/Contact">問い合わせ</a>
-          </nav>
-      </div>
-    </div>
-  </template>
+          </v-btn>
+          <v-btn 
+            href="/privacy-policy"
+            variant="text"
+            rounded="xl" >
+            プライバシーポリシー
+          </v-btn>
+          <v-btn 
+            href="/terms"
+            variant="text"
+            rounded="xl" >
+            利用規約
+          </v-btn>
+          <v-btn 
+            href="/contact"
+            variant="text"
+            rounded="xl" >
+            お問い合わせ
+          </v-btn>
+          <v-col class="text-center mt-4" cols="12">
+            {{ new Date().getFullYear() }} — かえで
+          </v-col>
+        </v-row>
+      </v-footer>
+    </v-app>
+</template>
   
-  <style>
-  /* footer {
-    display: flex;
-    justify-content: center;
-    line-height: 1.5;
-  } */
-  footer {
-    left: 0;
-    width: 100%;
-    background-color: white;
-  }
-  
-  .wrapper {
-    position: fixed;
-    bottom: 0;
-    width: 100%;
-    display: flex;
-    justify-content: center;
-    line-height: 1.5;
-    background-color: rgb(145, 144, 230);
-    /* max-height: 100vh; */
-  }
-  
-  nav a.router-link-exact-active {
-    color: var(--color-text);
-  }
-  
-  nav a.router-link-exact-active:hover {
-    background-color: transparent;
-  }
-  
-  nav a {
-    display: inline-block;
-    padding: 0 1rem;
-    border-left: 1px solid var(--color-border);
-  }
-  
-  nav a:first-of-type {
-    border: 0;
-  }
-  
-  @media (min-width: 1024px) {
-    /* footer {
-      display: flex;
-      justify-content: center;
-      padding-right: calc(var(--section-gap) / 2);
-    } */
-    
-    .wrapper {
-    width: 100%;
-    display: flex;
-    justify-content: center;
-    line-height: 1.5;
-    max-height: 100vh;
-    }
-  
-    nav {
-      text-align: center;
-      /* margin-left: -1rem; */
-      font-size: 1rem;
-  
-      padding: 1rem 0;
-      margin-top: 1rem;
-    }
-  }
-  </style>
+<style>
+</style>
   
