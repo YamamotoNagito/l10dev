a155f6a4cf60ab984d5b074de9876c878107fa35
yuki <b202154@hiroshima-u.ac.jp>
2023-12-29 17:04:07 +0900

    Chart.jsのバージョンを最新のものに修正

--- resources/components/RadarChart.vue
+++ resources/components/RadarChart.vue
@@ -8,7 +8,7 @@ const loadChartJS = () => {
   return new Promise((resolve, reject) => {
     const scriptChartJS = document.createElement("script");
     scriptChartJS.src =
-      "https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.7.0/chart.js"; // Update the Chart.js version if needed
+      "https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.4.1/chart.js"; // Update the Chart.js version if needed
     scriptChartJS.defer = true;
     document.head.appendChild(scriptChartJS);
 
