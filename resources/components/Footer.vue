d88e4e411b4bad687c3b3fd8f2f84dce6b353c67
@amoon46 <atsukih46@gmail.com>
2024-01-07 06:25:57 +0900

    add-absolute

--- resources/components/Footer.vue
+++ resources/components/Footer.vue
@@ -1,26 +1,26 @@
 <template>
     <v-app>
-      <v-footer class="bg-grey-lighten-1" app>
+      <v-footer class="bg-grey-lighten-1" app absolute>
         <v-row justify="center" no-gutters>
-          <v-btn 
+          <v-btn
             href="/about-us"
             variant="text"
             rounded="xl" >
             Developed by SOS団
           </v-btn>
-          <v-btn 
+          <v-btn
             href="/privacy-policy"
             variant="text"
             rounded="xl" >
             プライバシーポリシー
           </v-btn>
-          <v-btn 
+          <v-btn
             href="/terms"
             variant="text"
             rounded="xl" >
             利用規約
           </v-btn>
-          <v-btn 
+          <v-btn
             href="/contact"
             variant="text"
             rounded="xl" >
@@ -33,7 +33,6 @@
       </v-footer>
     </v-app>
 </template>
-  
+
 <style>
 </style>
-  
