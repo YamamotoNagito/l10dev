665feeafb62960aca9fda199b0e68d1cef912d8f
Kosei Tanaka <k-sei0213@hotmail.com>
2024-01-10 23:29:56 +0900

    お問い合わせ画面をつくった

--- resources/components/Contact.vue
+++ resources/components/Contact.vue
@@ -9,10 +9,55 @@ import { useRouter } from 'vue-router'
 <template>
   <v-app>
       <v-main>
-        <v-container class="bg-green-lighten-3 rounded-lg" height="400px">
-          <p>ここに記述をしていく</p>
-          <p>ClassList</p>
+        <v-container>
+            <p class="text-h6 text-md-h4 text-lg-h3">お問い合わせ</p>
+            <br/>
+            <p class="text-h6 text-md-h5 text-lg-h4">ユーザ情報</p>
+            <v-text-field
+              label="氏名"
+              placeholder="広島 かえで"
+              hide-details="auto"
+            ></v-text-field>
+            <v-text-field
+              label="メールアドレス"
+              placeholder="Kaede@gmail.com"
+              type="email"
+            ></v-text-field>
+            <p class="text-h6 text-md-h5 text-lg-h4">カテゴリ</p>
+            <v-combobox
+                v-model="select"
+                :items="category_items"
+                label="お問い合わせ内容に最も合うものを選んでください. "
+                clearable
+            ></v-combobox>
+            <p class="text-h6 text-md-h5 text-lg-h4">お問い合わせ内容</p>
+            <v-container fluid>
+                <v-textarea
+                name="input-7-1"
+                filled
+                label="こちらにお問い合わせ内容を記述してください. "
+                auto-grow
+                ></v-textarea>
+            </v-container>
+            <v-btn color="indigo">送信する</v-btn>
+
+
         </v-container>
       </v-main>
   </v-app>
 </template>
+<script>
+  export default {
+    data () {
+      return {
+        select: [],
+        category_items: [
+          '選択肢１',
+          '選択肢２',
+          '選択肢３',
+          'その他',
+        ],
+      }
+    },
+  }
+</script>
