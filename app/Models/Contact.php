78ff63db9d7a38dcd0103748ce03a8ac80345578
Atsuki Hattori <74179935+amoon46@users.noreply.github.com>
2024-01-11 19:56:36 +0900

    Revert "add 'use Illuminate\Database\Eloquent\Model'"

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -2,12 +2,12 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use Spatie\Permission\Traits\HasRoles;
+use Illuminate\Database\Eloquent\Model;
 
 class Contact extends Model
 {
@@ -15,7 +15,7 @@ class Contact extends Model
 
     protected $table = 'contact';
     protected $primaryKey = 'contact_id';
-
+    
     // public $timestamps = true;
     public $timestamps = false; // 自動更新を無効にする
 
--- app/Models/DeleteReviewRequests.php
+++ app/Models/DeleteReviewRequests.php
@@ -2,13 +2,11 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use Spatie\Permission\Traits\HasRoles;
-
 
 class DeleteReviewRequests extends Model
 {
