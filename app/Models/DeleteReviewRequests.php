0c224c9b323f213f03f8d9f1c2d7edaadd56cd47
@amoon46 <atsukih46@gmail.com>
2024-01-11 19:54:18 +0900

    add-use Illuminate\Database\Eloquent\Model

--- app/Models/DeleteReviewRequests.php
+++ app/Models/DeleteReviewRequests.php
@@ -2,11 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use Spatie\Permission\Traits\HasRoles;
+
 
 class DeleteReviewRequests extends Model
 {
