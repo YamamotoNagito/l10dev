97bf8d9f056b00c0da9055fbd4042db82e744a96
@amoon46 <atsukih46@gmail.com>
2024-01-11 19:42:07 +0900

    tmp commit

--- app/Models/Reviews.php
+++ app/Models/Reviews.php
@@ -7,6 +7,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use Spatie\Permission\Traits\HasRoles;
+use Illuminate\Database\Eloquent\Model;
 
 class Reviews extends Model
 {
@@ -15,4 +16,28 @@ class Reviews extends Model
     protected $table = 'reviews';
 
     protected $primaryKey = 'review_id';
+
+    public $timestamps = false;
+
+    // モデルのJSON形式に含める属性
+    protected $visible = [
+      'review_id',
+      'lecture_id',
+      'user_id',
+      'attendance_year',
+      'attendance_confirm',
+      'weekly_assignments',
+      'midterm_assignments',
+      'final_assignments',
+      'past_exam_possession',
+      'grades',
+      'credit_level',
+      'interest_level',
+      'skill_level',
+      'comments',
+      'created_at',
+      'updated_at',
+      'is_visible',
+  ];
 }
+
