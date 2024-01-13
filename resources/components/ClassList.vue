f3a0a62585055d945a7f09b61bc073f4f3151f23
yuki <b202154@hiroshima-u.ac.jp>
2024-01-14 07:15:04 +0900

    授業一覧のpaginationを実現した

--- resources/components/ClassList.vue
+++ resources/components/ClassList.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, defineProps } from "vue";
+import { ref, defineProps, computed } from "vue";
 import vuetify from "../js/vuetify";
 import axios from "axios";
 import { useRouter } from "vue-router";
@@ -7,6 +7,20 @@ import ClassSummary from "./ClassSummary.vue";
 import SearchClasses from "./SearchClasses.vue";
 
 const props = defineProps(["classDataList"])
+
+const classDataList2 = ref(props.classDataList)
+// const displayedItems = ref(props.classDataList)
+const itemsPerPage = 5;
+const currentPage = ref(1);
+
+const totalPages = computed(() =>
+  Math.ceil(classDataList2.value.length / itemsPerPage)
+);
+const displayedItems = computed(() => {
+  const start = (currentPage.value - 1) * itemsPerPage;
+  const end = start + itemsPerPage;
+  return classDataList2.value.slice(start, end);
+});
 </script>
 
 <template>
@@ -17,8 +31,14 @@ const props = defineProps(["classDataList"])
 
   <!-- ここから検索で出てきた授業のリスト -->
   <v-container class="d-flex flex-column align-center ga-5">
-    <v-container v-for="n in classDataList.length" :key="n">
-      <ClassSummary class="mx-auto" :classData="classDataList[n - 1]"></ClassSummary>
+    <v-container v-for="n in displayedItems.length" :key="n">
+      <ClassSummary class="mx-auto" :classData="displayedItems[n - 1]"></ClassSummary>
     </v-container>
   </v-container>
+
+  <v-pagination
+      v-model="currentPage"
+      :length="totalPages"
+      :totalVisible="5"
+    ></v-pagination>
 </template>
