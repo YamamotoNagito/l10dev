7469328746d70f8650bf9a612375ccd2b87cfe24
yuki <b202154@hiroshima-u.ac.jp>
2024-01-14 18:21:15 +0900

    以上と未満で評価項目の検索ができるようになった

--- resources/components/SearchClasses.vue
+++ resources/components/SearchClasses.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref } from "vue";
+import { ref, computed } from "vue";
 import { useRouter } from "vue-router";
 import axios from "axios";
 
@@ -18,11 +18,63 @@ const detailedCondition = ref({
   dayOfWeek: null,
   timePeriod: null,
   grade: null,
-  totalEvaluation: null,
-  creditLevel: null,
-  interestLevel: null,
-  skillLevel: null,
+  totalEvaluation: {
+    min: null,
+    max: null,
+  },
+  creditLevel: {
+    min: null,
+    max: null,
+  },
+  interestLevel: {
+    min: null,
+    max: null,
+  },
+  skillLevel: {
+    min: null,
+    max: null,
+  },
 });
+// プルダウン内の文字列（1未満，など）これはupdateEvaluationObject関数で適切なオブジェクトに変換される
+const totalEvaluationString = ref(null);
+const creditLevelString = ref(null);
+const interestLevelString = ref(null);
+const skillLevelString = ref(null);
+
+// プルダウン内の文字列から，minとmaxをキーにもつオブジェクトを生成し，そのオブジェクトを返す関数
+const updateEvaluationObject = (evaluationString) => {
+  const evaluationObj = {};
+  switch (evaluationString) {
+    case "1未満":
+      evaluationObj.min = 0;
+      evaluationObj.max = 1;
+      break;
+    case "1以上":
+      evaluationObj.min = 1;
+      evaluationObj.max = 2;
+      break;
+    case "2以上":
+      evaluationObj.min = 2;
+      evaluationObj.max = 3;
+      break;
+    case "3以上":
+      evaluationObj.min = 3;
+      evaluationObj.max = 4;
+      break;
+    case "4以上":
+      evaluationObj.min = 4;
+      evaluationObj.max = 5;
+      break;
+
+    default:
+      // デフォルトの処理はnullになる
+      evaluationObj.min = null
+      evaluationObj.max = null
+      break;
+  }
+  console.log(evaluationObj)
+  return evaluationObj
+};
 
 // TODO
 // 検索項目はフロントでもつ
@@ -47,6 +99,12 @@ const nonExistenceMessage = ref("");
 // 条件で検索するボタンが押されたときに発火する関数
 //welcome内の検索機能と一覧内の検索機能を同じにしようとしたら，検索条件を/classに送信して，/class内で検索
 const sendQueryToClassListView = async () => {
+  // プルダウンの文字列からオブジェクトを生成し，datailedConditionに格納する
+  detailedCondition.value.totalEvaluation = updateEvaluationObject(totalEvaluationString.value)
+  detailedCondition.value.creditLevel = updateEvaluationObject(creditLevelString.value)
+  detailedCondition.value.interestLevel = updateEvaluationObject(interestLevelString.value)  
+  detailedCondition.value.skillLevel = updateEvaluationObject(skillLevelString.value)
+
   console.log(detailedCondition.value);
   router.push({
     path: "/class",
@@ -54,16 +112,20 @@ const sendQueryToClassListView = async () => {
       lectureName: detailedCondition.value.lectureName,
       teacherName: detailedCondition.value.teacherName,
       location: detailedCondition.value.location,
-      faculty : detailedCondition.value.faculty,
-      category : detailedCondition.value.category,
-      term : detailedCondition.value.term,
-      dayOfWeek : detailedCondition.value.term,
-      timePeriod : detailedCondition.value.timePeriod,
-      grade : detailedCondition.value.grade,
-      totalEvaluation : detailedCondition.value.totalEvaluation,
-      creditLevel : detailedCondition.value.creditLevel,
-      interestLevel : detailedCondition.value.interestLevel,
-      skillLevel : detailedCondition.value.skillLevel
+      faculty: detailedCondition.value.faculty,
+      category: detailedCondition.value.category,
+      term: detailedCondition.value.term,
+      dayOfWeek: detailedCondition.value.term,
+      timePeriod: detailedCondition.value.timePeriod,
+      grade: detailedCondition.value.grade,
+      totalEvaluationMin: detailedCondition.value.totalEvaluation.min,
+      totalEvaluationMax: detailedCondition.value.totalEvaluation.max,
+      creditLevelMin: detailedCondition.value.creditLevel.min,
+      creditLevelMax: detailedCondition.value.creditLevel.max,
+      interestLevelMin: detailedCondition.value.interestLevel.min,
+      interestLevelMax: detailedCondition.value.interestLevel.max,
+      skillLevelMin: detailedCondition.value.skillLevel.min,
+      skillLevelMax: detailedCondition.value.skillLevel.max,
     },
   });
 };
@@ -214,9 +276,7 @@ const searchByLectureCode = async () => {
                         <p class="category-name">総合評価</p>
                         <v-select
                           :items="totalEvaluationList"
-                          v-model="
-                            detailedCondition.totalEvaluation
-                          "
+                          v-model="totalEvaluationString"
                           class="pulldown-list"
                         ></v-select>
                       </v-container>
@@ -225,7 +285,7 @@ const searchByLectureCode = async () => {
                         <p class="category-name">単位取得のしやすさ</p>
                         <v-select
                           :items="totalEvaluationList"
-                          v-model="detailedCondition.creditLevel"
+                          v-model="creditLevelString"
                           class="pulldown-list"
                         ></v-select>
                       </v-container>
@@ -234,27 +294,7 @@ const searchByLectureCode = async () => {
                         <p class="category-name">面白さ</p>
                         <v-select
                           :items="totalEvaluationList"
-                          v-model="detailedCondition.interestLevel"
-                          class="pulldown-list"
-                        ></v-select>
-                      </v-container>
-
-                      <v-container class="category-name-and-content-container">
-                        <p class="category-name">教員の質</p>
-                        <v-select
-                          :items="totalEvaluationList"
-                          v-model="
-                            detailedCondition.qualityOfTeacher
-                          "
-                          class="pulldown-list"
-                        ></v-select>
-                      </v-container>
-
-                      <v-container class="category-name-and-content-container">
-                        <p class="category-name">サポート体制</p>
-                        <v-select
-                          :items="totalEvaluationList"
-                          v-model="detailedCondition.support"
+                          v-model="interestLevelString"
                           class="pulldown-list"
                         ></v-select>
                       </v-container>
@@ -263,7 +303,7 @@ const searchByLectureCode = async () => {
                         <p class="category-name">スキルが身につくか</p>
                         <v-select
                           :items="totalEvaluationList"
-                          v-model="detailedCondition.skillLevel"
+                          v-model="skillLevelString"
                           class="pulldown-list"
                         ></v-select>
                       </v-container>
