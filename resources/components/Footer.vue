52a3f4d147d985e11936575a63e2e664407acdd6
Kosei Tanaka <k-sei0213@hotmail.com>
2024-01-14 18:05:24 +0900

    画面サイズに応じて段組みを作った

--- resources/components/Footer.vue
+++ resources/components/Footer.vue
@@ -1,34 +1,44 @@
 <template>
     <!-- <v-app> -->
       <v-footer class="bg-grey-lighten-1" app absolute>
-        <v-row justify="center" no-gutters>
-          <v-btn
-            href="/about-us"
-            variant="text"
-            rounded="xl" >
-            Developed by SOS団
-          </v-btn>
-          <v-btn
-            href="/privacy-policy"
-            variant="text"
-            rounded="xl" >
-            プライバシーポリシー
-          </v-btn>
-          <v-btn
-            href="/terms"
-            variant="text"
-            rounded="xl" >
-            利用規約
-          </v-btn>
-          <v-btn
-            href="/contact"
-            variant="text"
-            rounded="xl" >
-            お問い合わせ
-          </v-btn>
-          <v-col class="text-center mt-4" cols="12">
-            {{ new Date().getFullYear() }} — かえで
+        <v-row no-gutters>
+          <v-col cols="0" sm="0" md="2" lg="2" xl="2"></v-col>
+          <v-col cols="6" sm="3" md="2" lg="2" xl="2" class="d-flex justify-center" >
+            <v-btn
+              href="/about-us"
+              variant="text"
+              rounded="xl" >
+              Developed by SOS団
+            </v-btn>
           </v-col>
+          <v-col cols="6" sm="3" md="2" lg="2" xl="2" class="d-flex justify-center" >
+            <v-btn
+              href="/terms"
+              variant="text"
+              rounded="xl" >
+              利用規約
+            </v-btn>
+          </v-col>
+          <v-col cols="6" sm="3" md="2" lg="2" xl="2" class="d-flex justify-center" >
+            <v-btn
+              href="/privacy-policy"
+              variant="text"
+              rounded="xl" >
+              プライバシーポリシー
+            </v-btn>
+          </v-col>
+          <v-col cols="6" sm="3" md="2" lg="2" xl="2" class="d-flex justify-center" >
+            <v-btn
+              href="/contact"
+              variant="text"
+              rounded="xl" >
+              お問い合わせ
+            </v-btn>
+          </v-col>
+          <v-col cols="0" sm="0" md="2" lg="2" xl="2"></v-col>
+            <v-col class="text-center mt-4" cols="12">
+              {{ new Date().getFullYear() }} — かえで
+            </v-col>
         </v-row>
       </v-footer>
     <!-- </v-app> -->
