ae9f3c57318b628d4f3555a9bdc3ae979b6753f1
ueda1020 <uedaakio6380@gmail.com>
2024-02-12 18:05:08 +0900

    基本情報ページ内のグラフタブの名称変更

--- resources/components/GraphTabs.vue
+++ resources/components/GraphTabs.vue
@@ -7,12 +7,12 @@ const tab = ref(null);
 const props = defineProps(["classBarGraphData"])
 
 const graphTitleList = ref({
-    grades:"成績",
-    attendanceConfirm:"出欠",
-    pastExamPossesion:"過去問",
-    weeklyAssignments:"日課",
-    midtermAssignments:"中間",
-    finalAssignments:"期末",
+    grades:"みんなの成績",
+    attendanceConfirm:"出欠確認",
+    pastExamPossesion:"過去問の普及率",
+    weeklyAssignments:"日課の課題・レポート",
+    midtermAssignments:"中間レポート・課題",
+    finalAssignments:"期末レポート・課題",
 })
 </script>
 
@@ -20,12 +20,12 @@ const graphTitleList = ref({
   <v-card class="d-flex flex-column">
     <v-container class="d-flex justify-center">
       <v-tabs v-model="tab">
-        <v-tab value="one">成績</v-tab>
-        <v-tab value="two">出欠</v-tab>
-        <v-tab value="three">過去問</v-tab>
-        <v-tab value="four">日課</v-tab>
-        <v-tab value="five">中間</v-tab>
-        <v-tab value="six">期末</v-tab>
+        <v-tab value="one">みんなの成績</v-tab>
+        <v-tab value="two">出欠確認</v-tab>
+        <v-tab value="three">過去問の普及率</v-tab>
+        <v-tab value="four">日課の課題・レポート</v-tab>
+        <v-tab value="five">中間レポート・課題</v-tab>
+        <v-tab value="six">期末レポート・課題</v-tab>
       </v-tabs>
     </v-container>
     <v-card-text>
