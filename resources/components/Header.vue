a1a5e14037617068c7e35d8a57be2ddf3e924866
amoon46 <atsukih46@gmail.com>
2024-02-21 20:50:46 +0900

    #339 ケバブボタン作成中

--- resources/components/Header.vue
+++ resources/components/Header.vue
@@ -49,50 +49,90 @@
         console.log(err);
       });
   };
+
+  const items = ref([
+    { title: "プロフィール", action: "profile" },
+    { title: "ログアウト", action: "logout" }
+  ]);
 </script>
 <template>
-  <v-app-bar color="primary" elevation="0" absolute height="80">
-    <!-- <v-btn icon @click="navigateToWelcome" id="kaede-icon-btn">
-      <img :src="kaedeMainImg" alt="kaedeMainImg" width="40" height="40" />
-    </v-btn> -->
+  <v-app-bar color="primary" elevation="0" absolute height="80" class="d-flex">
+    <v-row align="center">
+      <v-col cols="0" sm="0" md="1" lg="1" xl="1"> </v-col>
+      <v-col cols="2" sm="2" md="2" lg="2" xl="2">
+        <v-toolbar-title
+          class="text-white hidden-md-and-down"
+          style="cursor: pointer"
+          @click="router.push({ name: 'welcome' })"
+        >
+          かえで
+        </v-toolbar-title>
+      </v-col>
 
-    <v-app-bar-title
-      class="text-white hidden-md-and-down"
-      style="cursor: pointer"
-      @click="router.push({ name: 'welcome' })"
-    >
-      かえで
-    </v-app-bar-title>
-    <v-spacer />
-    <v-btn variant="text" color="background" @click="router.push({ name: 'welcome' })">
-      <v-icon>mdi-magnify</v-icon>
-      授業検索
-    </v-btn>
+      <v-col cols="8" sm="8" md="8" lg="8" xl="8" class="d-flex justify-end ml-auto">
+        <v-btn variant="text" color="background" @click="router.push({ name: 'welcome' })">
+          <v-icon>mdi-magnify</v-icon>
+          授業検索
+        </v-btn>
 
-    <v-btn variant="text" color="background" v-if="!store.getters.isLoggedIn" @click="router.push({ name: 'login' })">
-      <v-icon>mdi-login</v-icon>
-      ログイン
-    </v-btn>
+        <v-btn
+          variant="text"
+          color="background"
+          v-if="!store.getters.isLoggedIn"
+          @click="router.push({ name: 'login' })"
+        >
+          <v-icon>mdi-login</v-icon>
+          ログイン
+        </v-btn>
 
-    <v-btn
-      variant="text"
-      color="background"
-      v-if="store.getters.isLoggedIn"
-      @click="router.push({ name: 'class/post' })"
-    >
-      <v-icon>mdi-pencil</v-icon>
-      レビュー投稿
-    </v-btn>
+        <v-btn
+          variant="text"
+          color="background"
+          v-if="store.getters.isLoggedIn"
+          @click="router.push({ name: 'class/post' })"
+        >
+          <v-icon>mdi-pencil</v-icon>
+          レビュー投稿
+        </v-btn>
 
-    <v-btn variant="text" color="background" v-if="store.getters.isLoggedIn" @click="clickProfileButton">
-      <v-icon>mdi-account</v-icon>
-      プロフィール
-    </v-btn>
+        <v-btn v-if="store.getters.isLoggedIn" variant="text" color="white">
+          <v-icon>mdi-menu</v-icon>
+        </v-btn>
 
-    <v-btn variant="text" color="white" @click="clickLogoutButton">
-      <v-icon>mdi-logout</v-icon>
-      ログアウト
-    </v-btn>
+        <v-menu v-if="store.getters.isLoggedIn" activator="parent" location="start">
+          <v-list>
+            <v-list-item v-for="(item, index) in items" :key="index" :value="index" @click="handleMenuItemClick(item)">
+              <v-list-item-title :style="item.title === '削除する' ? 'color: red' : ''">{{
+                item.title
+              }}</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+
+        <v-dialog v-model="showDialog" persistent max-width="300px">
+          <v-card>
+            <v-card-title class="text-h5">確認</v-card-title>
+            <v-card-text>このレビューを削除してもよろしいですか？</v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="grey" text @click="showDialog = false">キャンセル</v-btn>
+              <v-btn color="red" text @click="deleteReview">削除</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+
+        <!-- <v-btn variant="text" color="background" v-if="store.getters.isLoggedIn" @click="clickProfileButton">
+          <v-icon>mdi-account</v-icon>
+          プロフィール
+        </v-btn> -->
+
+        <!-- <v-btn variant="text" color="white" @click="clickLogoutButton">
+          <v-icon>mdi-logout</v-icon>
+          ログアウト
+        </v-btn> -->
+      </v-col>
+      <v-col cols="0" sm="0" md="1" lg="1" xl="1"></v-col>
+    </v-row>
   </v-app-bar>
 
   <!-- ハンバーガーメニューの中身（サイドバー） -->
