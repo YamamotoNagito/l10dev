d40480d85d91ca2a8ebf0645c4a4c02b254a972c
amoon46 <atsukih46@gmail.com>
2024-02-22 00:27:16 +0900

    #346 Dialogに関する問題の解消

---
+++ resources/components/shared/DialogItem.vue
@@ -0,0 +1,39 @@
+<script setup>
+  import { defineProps, defineEmits } from "vue";
+
+  const props = defineProps({
+    showDialog: Boolean,
+    messageInDialog: String
+  });
+  const emits = defineEmits(["toggleShowDialog"]);
+
+  const closeDialog = () => {
+    emits("toggleShowDialog");
+  };
+</script>
+
+<template>
+  <v-dialog v-model="props.showDialog" persistent width="320px">
+    <v-card>
+      <v-row>
+        <v-col cols="1">
+          <v-card-text>
+            <v-icon icon="mdi-alert" color="red"></v-icon>
+          </v-card-text>
+        </v-col>
+        <v-col cols="11" class="pb-0">
+          <v-card-text>
+            <p class="text-center text-red">{{ messageInDialog }}</p>
+          </v-card-text>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col class="d-flex justify-center pt-0">
+          <v-btn class="mb-2" color="orange" flat @click="closeDialog">
+            <p class="text-white">OK</p>
+          </v-btn>
+        </v-col>
+      </v-row>
+    </v-card>
+  </v-dialog>
+</template>
