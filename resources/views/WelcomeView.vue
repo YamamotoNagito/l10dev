bfb1ad81722887ac0ac69fdf3f94d309ee25e6d9
amoon46 <atsukih46@gmail.com>
2024-02-24 03:54:05 +0900

    #340 不要なscriptを削除

--- resources/views/WelcomeView.vue
+++ resources/views/WelcomeView.vue
@@ -1,36 +1,7 @@
 <script setup>
-  import { ref, watch, onMounted } from "vue";
-  import { useDisplay } from "vuetify";
   import Welcome from "../components/Welcome.vue";
   import SearchClasses from "../components/SearchClasses.vue";
   import FontChangeTitle from "../components/shared/FontChangeTitle.vue";
-
-  const { xs, sm, md, lg, xl, xxl } = useDisplay();
-  const fontSize = ref("");
-
-  // text-h◯とfontの変更は同時に使用できないため，fontSizeを使用する
-  const setFontSize = () => {
-    if (xxl.value) {
-      fontSize.value = "2.8rem";
-    } else if (xl.value) {
-      fontSize.value = "2.4rem";
-    } else if (lg.value) {
-      fontSize.value = "2.0rem";
-    } else if (md.value) {
-      fontSize.value = "1.8rem";
-    } else if (sm.value) {
-      fontSize.value = "1.5rem";
-    } else if (xs.value) {
-      fontSize.value = "1.2rem";
-    } else {
-      fontSize.value = "0.8rem";
-    }
-  };
-
-  onMounted(setFontSize);
-
-  // ブレークポイントが変更されたときにフォントサイズを更新
-  watch([xs, sm, md, lg, xl, xxl], setFontSize);
 </script>
 
 <template>
