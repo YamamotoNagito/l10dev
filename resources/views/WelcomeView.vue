dc3eef911276d3fdbad5928e992d5df406771871
amoon46 <atsukih46@gmail.com>
2024-02-23 22:21:34 +0900

    #340 fontの変更

--- resources/views/WelcomeView.vue
+++ resources/views/WelcomeView.vue
@@ -1,8 +1,36 @@
 <script setup>
+  import { ref, watch, onMounted } from "vue";
+  import { useDisplay } from "vuetify";
   import Welcome from "../components/Welcome.vue";
   import SearchClasses from "../components/SearchClasses.vue";
-  import pageTitle from "../components/pageTitle.vue";
   import BaseUI from "../components/shared/BaseUI.vue";
+
+  const { xs, sm, md, lg, xl, xxl } = useDisplay();
+  const fontSize = ref("");
+
+  // text-h◯とfontの変更は同時に使用できないため，fontSizeを使用する
+  const setFontSize = () => {
+    if (xxl.value) {
+      fontSize.value = "2.8rem";
+    } else if (xl.value) {
+      fontSize.value = "2.4rem";
+    } else if (lg.value) {
+      fontSize.value = "2.0rem";
+    } else if (md.value) {
+      fontSize.value = "1.8rem";
+    } else if (sm.value) {
+      fontSize.value = "1.5rem";
+    } else if (xs.value) {
+      fontSize.value = "1.2rem";
+    } else {
+      fontSize.value = "0.8rem";
+    }
+  };
+
+  onMounted(setFontSize);
+
+  // ブレークポイントが変更されたときにフォントサイズを更新
+  watch([xs, sm, md, lg, xl, xxl], setFontSize);
 </script>
 
 <template>
@@ -10,7 +38,10 @@
     <template #header>
       <v-container class="pb-8 bg-primary" fluid>
         <div id="title-block">
-          <p class="text-white text-h6 text-md-h4 text-lg-h4">大学生による授業評価をオープンに</p>
+          <p :style="{ fontSize: fontSize, fontFamily: 'Kaisei' }" class="text-white">
+            大学生による授業評価を、オープンに。
+            <!-- ~広大生の学びを支える情報サイト~ -->
+          </p>
         </div>
       </v-container>
     </template>
