cdc9592c4ae6f6d5c0dd8b0a695dda865a950387
amoon46 <atsukih46@gmail.com>
2024-02-24 05:51:23 +0900

    #326 画面サイズに合わせて，pxを調整

--- resources/components/shared/pageTitle.vue
+++ resources/components/shared/pageTitle.vue
@@ -10,23 +10,23 @@
   // console.log("loaded.");
 
   const { xs, sm, md, lg, xl, xxl } = useDisplay();
-  const imageSize = ref("50px"); // 画像サイズの初期値
+  const imageSize = ref("50px");
 
   const setImageSize = () => {
     if (xxl.value) {
-      imageSize.value = "100px"; // XXLの場合の画像サイズ
+      imageSize.value = "90px";
     } else if (xl.value) {
-      imageSize.value = "90px"; // XLの場合の画像サイズ
+      imageSize.value = "80px";
     } else if (lg.value) {
-      imageSize.value = "80px"; // LGの場合の画像サイズ
+      imageSize.value = "70px";
     } else if (md.value) {
-      imageSize.value = "70px"; // MDの場合の画像サイズ
+      imageSize.value = "60px";
     } else if (sm.value) {
-      imageSize.value = "60px"; // SMの場合の画像サイズ
+      imageSize.value = "50px";
     } else if (xs.value) {
-      imageSize.value = "50px"; // XSの場合の画像サイズ
+      imageSize.value = "40px";
     } else {
-      imageSize.value = "40px"; // それ以外の場合の画像サイズ
+      imageSize.value = "30px";
     }
   };
 
