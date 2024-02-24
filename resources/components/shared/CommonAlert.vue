bee9025bce3036d44aff5a0f7a6825ba7e4d1e58
piro-hiroki <iwashita1112hiroki@gmail.com>
2024-02-24 13:14:58 +0900

    ✨ Add CommonAlert component and useMessage composable

---
+++ resources/components/shared/CommonAlert.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-alert
+    v-if="message"
+    :type="type"
+    class="my-6 alert-high-z-index"
+    closable
+    close-label="Close Alert"
+    location="top center"
+    position="fixed"
+    elevation="24"
+  >
+    {{ message }}
+  </v-alert>
+</template>
+
+<script>
+  export default {
+    props: {
+      message: {
+        type: String,
+        default: ""
+      },
+      type: {
+        type: String,
+        default: "info"
+      }
+    }
+  };
+</script>
+
+<style>
+  /* カスタムCSSクラスでz-indexを設定。ヘッダーと被らないようにするため。 */
+  .alert-high-z-index {
+    z-index: 10000;
+  }
+</style>
