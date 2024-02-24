25b386e753f278849623211e5654c39663e0183a
piro-hiroki <iwashita1112hiroki@gmail.com>
2024-02-24 13:24:36 +0900

    ✨ minWidthを追加

--- resources/components/shared/CommonAlert.vue
+++ resources/components/shared/CommonAlert.vue
@@ -2,7 +2,7 @@
   <v-alert
     v-if="message"
     :type="type"
-    class="my-6 alert-high-z-index"
+    class="my-6 alert-high-z-index responsive-min-width"
     closable
     close-label="Close Alert"
     location="top center"
@@ -32,5 +32,16 @@
   /* カスタムCSSクラスでz-indexを設定。ヘッダーと被らないようにするため。 */
   .alert-high-z-index {
     z-index: 10000;
+  }
+  /* デフォルトのmin-widthはPC用 */
+  .responsive-min-width {
+    min-width: 45rem;
+  }
+
+  /* 画面幅が768px以下の場合、スマホ用のmin-widthを適用 */
+  @media (max-width: 768px) {
+    .responsive-min-width {
+      min-width: 20rem;
+    }
   }
 </style>
