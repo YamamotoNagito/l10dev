9d56c7d52f026ae81e7c1310c37340a8433e4305
piro-hiroki <iwashita1112hiroki@gmail.com>
2024-02-25 23:59:33 +0900

    Fix typo

--- resources/views/NotFoundView.vue
+++ resources/views/NotFoundView.vue
@@ -2,7 +2,7 @@
   import { useRouter } from "vue-router";
   import pageTitle from "../components/pageTitle.vue";
   import BaseUI from "../components/shared/BaseUI.vue";
-  import NouFoundImage from "../assets/img/not_found.svg";
+  import NotFoundImage from "../assets/img/not_found.svg";
   const router = useRouter();
 </script>
 
@@ -12,7 +12,7 @@
       <pageTitle title="ページが存在しません" />
     </template>
     <v-container class="d-flex flex-column align-center justify-center">
-      <v-img :src="NouFoundImage" alt="not_found" width="700" class="mb-12" contain />
+      <v-img :src="NotFoundImage" alt="not_found" width="700" class="mb-12" contain />
       <v-btn rounded max-width="150px" class="mx-auto mt-5" @click="router.push({ name: 'welcome' })"
         >ホームに戻る</v-btn
       >
