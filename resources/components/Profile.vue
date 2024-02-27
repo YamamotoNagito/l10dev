e92ec490459e1d1263ac95b787086058f85431b9
amoon46 <atsukih46@gmail.com>
2024-02-27 10:03:51 +0900

    #171 tableを導入

--- resources/components/Profile.vue
+++ resources/components/Profile.vue
@@ -1,16 +1,58 @@
 <template>
   <v-row>
     <v-col cols="12">
-      <!-- タイトル -->
-      <!-- <h1 style="font-size: 2rem">プロフィール画面</h1> -->
       <v-card v-if="userProfile" class="my-8">
         <v-card-text justify>
-          <h1>{{ userProfile.userName }}</h1>
-          <p>Email: {{ userProfile.userEmail }}</p>
-          <p>Category: {{ userProfile.category }}</p>
-          <p>Faculty: {{ userProfile.faculty }}</p>
-          <p>Department: {{ userProfile.department }}</p>
-          <p>Admission Year: {{ userProfile.admissionYear }}</p>
+          <v-table>
+            <thead>
+              <tr>
+                <th class="text-left">項目</th>
+                <th class="text-left">情報</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>ユーザー名</td>
+                <td>{{ userProfile.userName }}</td>
+              </tr>
+              <tr>
+                <td>メールアドレス</td>
+                <td>{{ userProfile.userEmail }}</td>
+              </tr>
+              <tr>
+                <td>カテゴリー</td>
+                <td>{{ userProfile.category }}</td>
+              </tr>
+              <tr>
+                <td>学部</td>
+                <td>{{ userProfile.faculty }}</td>
+              </tr>
+              <tr>
+                <td>学科</td>
+                <td>{{ userProfile.department }}</td>
+              </tr>
+              <tr>
+                <td>入学年度</td>
+                <td>{{ userProfile.admissionYear }}</td>
+              </tr>
+              <!-- その他の情報 -->
+            </tbody>
+          </v-table>
+          <!--
+          <v-table>
+            <thead>
+              <tr>
+                <th class="text-left">Name</th>
+                <th class="text-left">Calories</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="item in desserts" :key="item.name">
+                <td>{{ item.name }}</td>
+                <td>{{ item.calories }}</td>
+              </tr>
+            </tbody>
+          </v-table> -->
         </v-card-text>
       </v-card>
       <v-card v-else>
@@ -19,7 +61,7 @@
         </v-card-text>
       </v-card>
       <!-- レビューリスト -->
-      <v-card v-if="reviewDataList" variant="0">
+      <v-card v-if="reviewDataList">
         <v-tabs v-model="tab" color="deep-purple-accent-4" align-tabs="left">
           <v-tab value="one">投稿数 {{ reviewDataList.length }} 件</v-tab>
           <!-- <v-tab value="two">Likes</v-tab> -->
