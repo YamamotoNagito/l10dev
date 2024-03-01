b1458f50f8b4570ae9a28c87bbbeb8e54dc16c93
amoon46 <atsukih46@gmail.com>
2024-03-02 02:48:02 +0900

    #394 簡単な説明を加えた

--- resources/components/Welcome.vue
+++ resources/components/Welcome.vue
@@ -1,10 +1,18 @@
 <script setup>
   import { ref } from "vue";
-
-  const message = ref("Hello World!");
 </script>
 
 <template>
-  <!-- <h1>ようこそ</h1> -->
-  <!-- <p>このサイトは広大生の学びを支えるサイト かえで</p> -->
+  <v-card max-width="900px" class="mx-auto my-16" elevation="0">
+    <v-card-title class="bg-primary d-flex align-center">
+      <p class="text-white"><v-icon outline="">mdi-help</v-icon>かえでとは</p>
+    </v-card-title>
+    <v-card-item>
+      <v-row justify="center">
+        <v-col cols="12" class="d-flex align-center">
+          <p class="text-center">大学の授業評価やテスト情報が見れるクチコミサイト</p>
+        </v-col>
+      </v-row>
+    </v-card-item>
+  </v-card>
 </template>
