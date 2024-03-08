420f25379cc38b3264df4957094d0553cbcc0ad1
Kosei Tanaka <k-sei0213@hotmail.com>
2024-03-08 23:22:33 +0900

    #394 簡単なレイアウト ブレークポイントとかは今度

--- resources/components/Welcome.vue
+++ resources/components/Welcome.vue
@@ -5,6 +5,37 @@
 </script>
 
 <template>
-  <!-- <h1>ようこそ</h1> -->
-  <!-- <p>このサイトは広大生の学びを支えるサイト かえで</p> -->
+  <v-container>
+    <!-- 検索機能の紹介 -->
+    <v-row>
+      <v-col cols="5"></v-col>
+      <v-col cols="7"></v-col>
+    </v-row>
+    <!-- 投稿機能の紹介(どのように反映されるかも) -->
+    <v-row>
+      <v-col cols="7"></v-col>
+      <v-col cols="5"></v-col>
+    </v-row>
+    <!-- シェア機能の紹介 -->
+    <v-row>
+      <v-col cols="7">
+        <img src="../assets/img/undraw_share.svg">
+      </v-col>
+      <v-col cols="5">
+        <p class="text-h1">気になる授業をシェアしよう</p>
+        <!-- <div class="text-h1">気になる授業を</div>
+        <div class="text-h1">シェアしよう</div> -->
+      </v-col>
+    </v-row>
+    <!-- 使ってみてね的なメッセージ -->
+    <v-row>
+      <v-col cols="5">
+        <p class="text-h1">あなたのご利用をお待ちしています！</p>
+      </v-col>
+      <v-col cols="7">
+        <img src="../assets/img/undraw_post.svg">
+      </v-col>
+    </v-row>
+    
+  </v-container>
 </template>
