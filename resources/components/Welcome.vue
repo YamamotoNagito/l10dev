1e6e5dbf85145e0ad3dd884e490c7bcba2f7bf8a
Kosei Tanaka <k-sei0213@hotmail.com>
2024-03-11 17:42:57 +0900

    #394 とりあえず画像と説明文を配置した.あとはサービスを改善しながら書き換える感じでよろしく.

--- resources/components/Welcome.vue
+++ resources/components/Welcome.vue
@@ -6,30 +6,50 @@
 
 <template>
   <v-container>
-    <!-- 検索機能の紹介 -->
+    <!-- 検索機能の紹介(星での検索を解禁したらつくる) -->
+    <!-- <v-row>
+      <v-col></v-col>
+    </v-row> -->
+    <!-- 閲覧機能の紹介 -->
     <v-row>
-      <v-col cols="5"></v-col>
-      <v-col cols="7"></v-col>
+      <v-col cols="5">
+        <!-- 画像ガビガビなのでもっといいやつに換えといてくれ -->
+        <img src="../assets/img/detail_info_rader.PNG" width="170%" height="170%">
+      </v-col>
+      <v-col cols="7" class="welcome-wrapper">
+        <div>
+          <p class="text-h1">
+            評価がグラフでわかる！
+          </p>
+          <p class="text-h2">
+            <br>
+            ・単位取得のしやすさ<br>
+            ・スキルが身につくか<br>
+            ・授業の面白さ
+          </p>
+        </div>
+      </v-col>
     </v-row>
-    <!-- 投稿機能の紹介(どのように反映されるかも) -->
     <v-row>
-      <v-col cols="7"></v-col>
-      <v-col cols="5"></v-col>
+      <v-col cols="5" class="welcome-wrapper">
+        <p class="text-h1">先輩のコメントも参考にしよう</p>
+      </v-col>
+      <v-col cols="7">
+        <img src="../assets/img/detail_reviews.PNG">
+      </v-col>
     </v-row>
     <!-- シェア機能の紹介 -->
     <v-row>
       <v-col cols="7">
         <img src="../assets/img/undraw_share.svg">
       </v-col>
-      <v-col cols="5">
+      <v-col cols="5" class="welcome-wrapper">
         <p class="text-h1">気になる授業をシェアしよう</p>
-        <!-- <div class="text-h1">気になる授業を</div>
-        <div class="text-h1">シェアしよう</div> -->
       </v-col>
     </v-row>
     <!-- 使ってみてね的なメッセージ -->
     <v-row>
-      <v-col cols="5">
+      <v-col cols="5" class="welcome-wrapper">
         <p class="text-h1">あなたのご利用をお待ちしています！</p>
       </v-col>
       <v-col cols="7">
@@ -39,3 +59,10 @@
     
   </v-container>
 </template>
+
+<style>
+.welcome-wrapper {
+  display: flex;
+  align-items: center; /*上下中央揃え*/
+}
+</style>
