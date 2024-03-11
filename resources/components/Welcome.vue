9cf7350c3a367ac3f6e176f1cbbafa779f5d6baa
piro-hiroki <iwashita1112hiroki@gmail.com>
2024-03-11 22:16:35 +0900

    Fix build error

--- resources/components/Welcome.vue
+++ resources/components/Welcome.vue
@@ -1,8 +1,4 @@
-<script setup>
-  import { ref } from "vue";
-
-  const message = ref("Hello World!");
-</script>
+<script setup></script>
 
 <template>
   <v-container>
@@ -14,17 +10,15 @@
     <v-row>
       <v-col cols="5">
         <!-- 画像ガビガビなのでもっといいやつに換えといてくれ -->
-        <img src="../assets/img/detail_info_rader.PNG" width="170%" height="170%">
+        <img src="../assets/img/detail_info_rader2.png" width="170%" height="170%" />
       </v-col>
       <v-col cols="7" class="welcome-wrapper">
         <div>
-          <p class="text-h1">
-            評価がグラフでわかる！
-          </p>
+          <p class="text-h1">評価がグラフでわかる！</p>
           <p class="text-h2">
-            <br>
-            ・単位取得のしやすさ<br>
-            ・スキルが身につくか<br>
+            <br />
+            ・単位取得のしやすさ<br />
+            ・スキルが身につくか<br />
             ・授業の面白さ
           </p>
         </div>
@@ -35,13 +29,13 @@
         <p class="text-h1">先輩のコメントも参考にしよう</p>
       </v-col>
       <v-col cols="7">
-        <img src="../assets/img/detail_reviews.PNG">
+        <img src="../assets/img/detail_reviews2.png" />
       </v-col>
     </v-row>
     <!-- シェア機能の紹介 -->
     <v-row>
       <v-col cols="7">
-        <img src="../assets/img/undraw_share.svg">
+        <img src="../assets/img/undraw_share.svg" />
       </v-col>
       <v-col cols="5" class="welcome-wrapper">
         <p class="text-h1">気になる授業をシェアしよう</p>
@@ -53,16 +47,15 @@
         <p class="text-h1">あなたのご利用をお待ちしています！</p>
       </v-col>
       <v-col cols="7">
-        <img src="../assets/img/undraw_post.svg">
+        <img src="../assets/img/undraw_post.svg" />
       </v-col>
     </v-row>
-    
   </v-container>
 </template>
 
 <style>
-.welcome-wrapper {
-  display: flex;
-  align-items: center; /*上下中央揃え*/
-}
+  .welcome-wrapper {
+    display: flex;
+    align-items: center; /*上下中央揃え*/
+  }
 </style>
