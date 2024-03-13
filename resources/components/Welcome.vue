f46d162bc91e2a512d5a098d531a556a375bfc81
amoon46 <atsukih46@gmail.com>
2024-03-13 16:59:46 +0900

    #471 一旦push

--- resources/components/Welcome.vue
+++ resources/components/Welcome.vue
@@ -1,4 +1,6 @@
-<script setup></script>
+<script setup>
+  import welcomeRadar from "../assets/img/welcomeInfoRadar.png";
+</script>
 
 <template>
   <v-container>
@@ -8,24 +10,23 @@
     </v-row> -->
     <!-- 閲覧機能の紹介 -->
     <v-row>
-      <v-col cols="5">
-        <!-- 画像ガビガビなのでもっといいやつに換えといてくれ -->
-        <img src="../assets/img/detail_info_rader2.png" width="170%" height="170%" />
-      </v-col>
-      <v-col cols="7" class="welcome-wrapper">
-        <div>
-          <p class="text-h1">評価がグラフでわかる！</p>
-          <p class="text-h2">
-            <br />
-            ・単位取得のしやすさ<br />
-            ・スキルが身につくか<br />
-            ・授業の面白さ
-          </p>
-        </div>
+      <v-col>
+        <v-row>
+          <v-col cols="12" sm="12" md="6" lg="6" xl="6" class="d-flex flex-column justify-center align-center">
+            <p class="text-h4 text-md-h3 text-lg-h3 text-center mb-5">評価がグラフでわかる！</p>
+
+            <p class="text-h5 text-md-h4 text-lg-h4 text-center">・単位取得のしやすさ</p>
+            <p class="text-h5 text-md-h4 text-lg-h4 text-center">・スキルが身につくか</p>
+            <p class="text-h5 text-md-h4 text-lg-h4 text-center">・授業の面白さ</p>
+          </v-col>
+          <v-col cols="12" sm="12" md="6" lg="6" xl="6">
+            <v-img :src="welcomeRadar" />
+          </v-col>
+        </v-row>
       </v-col>
     </v-row>
     <v-row>
-      <v-col cols="5" class="welcome-wrapper">
+      <v-col cols="5">
         <p class="text-h1">先輩のコメントも参考にしよう</p>
       </v-col>
       <v-col cols="7">
@@ -37,13 +38,13 @@
       <v-col cols="7">
         <img src="../assets/img/undraw_share.svg" />
       </v-col>
-      <v-col cols="5" class="welcome-wrapper">
+      <v-col cols="5">
         <p class="text-h1">気になる授業をシェアしよう</p>
       </v-col>
     </v-row>
     <!-- 使ってみてね的なメッセージ -->
     <v-row>
-      <v-col cols="5" class="welcome-wrapper">
+      <v-col cols="5">
         <p class="text-h1">あなたのご利用をお待ちしています！</p>
       </v-col>
       <v-col cols="7">
@@ -52,10 +53,3 @@
     </v-row>
   </v-container>
 </template>
-
-<style>
-  .welcome-wrapper {
-    display: flex;
-    align-items: center; /*上下中央揃え*/
-  }
-</style>
